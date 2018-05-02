09bc3c34e27f72f87c4493ffe8241102b7b48ccb
GuiGoVaz <32821235+GuiGoVaz@users.noreply.github.com>
2018-05-02 06:00:10 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,27 @@
+//Guilherme Gomes Vaz de Souza 11711ECP009
+#include <stdio.h>
+
+
+
+
+int main()
+{
+    printf("Digite uma sequencia: ");
+    char  bits[256];
+    int i=0,cum=0;
+    scanf("%s",&bits);
+    getchar();
+    while(bits[i]!='\0'){
+        if(bits[i]>='0' && bits[i]<='9'){
+            cum=cum*10+(bits[i]-'0');
+        }
+        i++;
+    }
+    printf("\nResultado: %d",cum);
+
+
+
+
+    return 0;
+    }
+
